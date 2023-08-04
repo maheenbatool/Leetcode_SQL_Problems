add2ccf10448024f9159cff13480b86e8c84ebeb
Maheen Batool <39929967+maheenbatool@users.noreply.github.com>
2023-08-04 22:19:28 +0100

    Create CustomersWhoBoughtAllProducts.sql

---
+++ CustomersWhoBoughtAllProducts.sql
@@ -0,0 +1,9 @@
+SELECT                                      
+    customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING SUM(DISTINCT product_key) = (
+    SELECT
+        SUM(product_key)
+    FROM Product
+); 
