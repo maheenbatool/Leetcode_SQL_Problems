54cd529cedf3c2f30d7aaa4ad71def59a933f13c
Maheen Batool <39929967+maheenbatool@users.noreply.github.com>
2023-08-06 23:45:34 +0100

    Create 602WhoHasMostFriends.sql

---
+++ 602WhoHasMostFriends.sql
@@ -0,0 +1,8 @@
+with cte as 
+(select requester_id id from RequestAccepted
+union all
+select accepter_id id from RequestAccepted)
+select id,count(*) num from cte
+group by id
+order by num desc
+limit 1
